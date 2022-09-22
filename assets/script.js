3b60fa7383242dc55a700e3fb4c089bf4c02764a
Daven <davensteen@gmail.com>
2022-09-22 00:00:11 -0500

    add initial pseudcode

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//get city name and store in local storage
+
+//get city weather
+
+//display weather info
+
+//reload city search history when clicked
+
+//add event listener to submit button and run getusername
+
+//add eventlistener to reload city from search history
